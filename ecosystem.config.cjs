c6420430dc63b89463770cba79d7fee71e999bb0
poornachander <chander.raparti@gmail.com>
2021-07-29 20:30:10 +0530

    changed ecosystem,package.json files
    content modified

---
+++ ecosystem.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+      name: "app",
+      script: "./server/server.js",
+      node_args: '--experimental-specifier-resolution=node',
+     
+    }]
+  }
--- server/express.js
+++ server/express.js
@@ -42,7 +42,7 @@ app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
 
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+// app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 // mount routes
 app.use('/', userRoutes)
